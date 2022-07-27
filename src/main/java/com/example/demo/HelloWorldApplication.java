73f70167acb82364fc4883a9a1c2eacdde79c56e
nk400 <navkr007@gmail.com>
2022-07-28 01:01:46 +0530

    ADDED: Used MVC Controller to invoke a view, passed Model and Used Thymeleaf to display the Model

--- src/main/java/com/example/demo/HelloWorldApplication.java
+++ src/main/java/com/example/demo/HelloWorldApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		System.out.print("Hello world form bridgelabz");
+		System.out.print("Hello world form BridgeLabz");
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
-
 }
